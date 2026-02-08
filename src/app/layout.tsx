dd2b85dc2efa53d9220bc49b93148bacf72d69f3
Vishal Biradar <biradarvishal290@gmail.com>
2026-02-08 21:41:00 +0000

    Try fixing this error: `Console Error: A tree hydrated but some attribut

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
@@ -40,6 +40,7 @@ export default function RootLayout({
           fontInter.variable,
           fontPlayfairDisplay.variable
         )}
+        suppressHydrationWarning={true}
       >
         {children}
         <Toaster />
